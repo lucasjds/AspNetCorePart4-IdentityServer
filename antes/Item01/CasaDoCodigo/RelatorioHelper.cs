a3b9a2b8849ebd6b9e678476f9e80649313d1528
lucas <lucassouza690@gmail.com>
2021-01-23 09:42:09 -0300

    Aula 2 - Consumindo Web API a Partir da Aplicação MVC

--- antes/Item01/CasaDoCodigo/RelatorioHelper.cs
+++ antes/Item01/CasaDoCodigo/RelatorioHelper.cs
@@ -1,63 +1,71 @@
 ﻿using CasaDoCodigo.Models;
+using Newtonsoft.Json;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CasaDoCodigo
 {
-    public interface IRelatorioHelper
+  public interface IRelatorioHelper
+  {
+    Task GerarRelatorio(Pedido pedido);
+  }
+
+  public class RelatorioHelper : IRelatorioHelper
+  {
+    public async Task GerarRelatorio(Pedido pedido)
     {
-        Task GerarRelatorio(Pedido pedido);
+      string linhaRelatorio = await GetLinhaRelatorio(pedido);
+      using (HttpClient httpClient = new HttpClient())
+      {
+        var json = JsonConvert.SerializeObject(linhaRelatorio);
+        HttpContent httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+      }
+
+
     }
 
-    public class RelatorioHelper : IRelatorioHelper
+    private async Task<string> GetLinhaRelatorio(Pedido pedido)
     {
-        public async Task GerarRelatorio(Pedido pedido)
-        {
-            string linhaRelatorio = await GetLinhaRelatorio(pedido);
-            await System.IO.File.AppendAllLinesAsync("Relatorio.txt", new string[] { linhaRelatorio });
-        }
+      StringBuilder sb = new StringBuilder();
+      string templatePedido =
+              await System.IO.File.ReadAllTextAsync("TemplatePedido.txt");
 
-        private async Task<string> GetLinhaRelatorio(Pedido pedido)
-        {
-            StringBuilder sb = new StringBuilder();
-            string templatePedido =
-                    await System.IO.File.ReadAllTextAsync("TemplatePedido.txt");
+      string templateItemPedido =
+          await System.IO.File.ReadAllTextAsync("TemplateItemPedido.txt");
 
-            string templateItemPedido =
-                await System.IO.File.ReadAllTextAsync("TemplateItemPedido.txt");
+      string linhaPedido =
+          string.Format(templatePedido,
+              pedido.Id,
+              pedido.Cadastro.Nome,
+              pedido.Cadastro.Endereco,
+              pedido.Cadastro.Complemento,
+              pedido.Cadastro.Bairro,
+              pedido.Cadastro.Municipio,
+              pedido.Cadastro.UF,
+              pedido.Cadastro.Telefone,
+              pedido.Cadastro.Email,
+              pedido.Itens.Sum(i => i.Subtotal));
 
-            string linhaPedido =
-                string.Format(templatePedido,
-                    pedido.Id,
-                    pedido.Cadastro.Nome,
-                    pedido.Cadastro.Endereco,
-                    pedido.Cadastro.Complemento,
-                    pedido.Cadastro.Bairro,
-                    pedido.Cadastro.Municipio,
-                    pedido.Cadastro.UF,
-                    pedido.Cadastro.Telefone,
-                    pedido.Cadastro.Email,
-                    pedido.Itens.Sum(i => i.Subtotal));
+      sb.AppendLine(linhaPedido);
 
-            sb.AppendLine(linhaPedido);
+      foreach (var i in pedido.Itens)
+      {
+        string linhaItemPedido =
+            string.Format(
+                templateItemPedido,
+                i.Produto.Codigo,
+                i.PrecoUnitario,
+                i.Produto.Nome,
+                i.Quantidade,
+                i.Subtotal);
 
-            foreach (var i in pedido.Itens)
-            {
-                string linhaItemPedido =
-                    string.Format(
-                        templateItemPedido,
-                        i.Produto.Codigo,
-                        i.PrecoUnitario,
-                        i.Produto.Nome,
-                        i.Quantidade,
-                        i.Subtotal);
+        sb.AppendLine(linhaItemPedido);
+      }
+      sb.AppendLine($@"=============================================");
 
-                sb.AppendLine(linhaItemPedido);
-            }
-            sb.AppendLine($@"=============================================");
-
-            return sb.ToString();
-        }
+      return sb.ToString();
     }
+  }
 }
