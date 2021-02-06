b0a9faf70dfc2cc2d5eb95efc3df2e42633b8dde
lucas <lucassouza690@gmail.com>
2021-02-06 12:00:12 -0300

    Aula 5 - Criando ContaController e Actions de Login e Logout

---
+++ antes/Item01/CasaDoCodigo/Controllers/ContaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+  public class ContaController : Controller
+  {
+    [Authorize]
+    public async Task<ActionResult> Login()
+    {
+      return Redirect(Url.Content("~/"));
+    }
+
+    [Authorize]
+    public async Task Logout()
+    {
+      //atualizar os cookies (autenticacao local)
+      await HttpContext.SignOutAsync("Cookies");
+      //faz o signout no identityserver
+      await HttpContext.SignOutAsync("OpenIdConnect");
+    }
+  }
+}
