ca0cf6b9644abab8baddcc0c71fbe50d4d97d770
lucas <lucassouza690@gmail.com>
2021-02-07 11:38:24 -0300

    Aula 6 - Protegendo Acesso ao Relatório de Vendas

--- antes/Item01/CasaDoCodigo.RelatorioWebApi1/Controllers/RelatorioController.cs
+++ antes/Item01/CasaDoCodigo.RelatorioWebApi1/Controllers/RelatorioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -36,6 +37,7 @@ namespace CasaDoCodigo.RelatorioWebApi1.Controllers
     }
 
     // POST api/<RelatorioController>
+    [Authorize]
     [HttpPost]
     public void Post([FromBody] string value)
     {
